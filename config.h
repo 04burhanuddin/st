399e8399d35fc5b5800f0f6d66d43ecada33db79
04burhanuddin <dev.burhanuddin@gmail.com>
2023-07-16 18:33:15 +0700

    update font2

--- config.h
+++ config.h
@@ -8,8 +8,7 @@
 static char *font = "JetbrainsMono Nerd Font:pixelsize=15:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-  "Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true",
-/* "Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+  "JetbrainsMono Nerd Font:pixelsize=12:antialias=true:autohint=true",
 };
 
 static int borderpx = 10;
