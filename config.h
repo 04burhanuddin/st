e3086a1c1677b2b7359591bb1c1461a118801b77
04burhanuddin <dev.burhanuddin@gmail.com>
2023-07-23 06:43:58 +0700

    change bg alpha

--- config.h
+++ config.h
@@ -113,7 +113,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.9;
+float alpha = 0.8;
 
 typedef struct {
 	const char* const colors[258]; /* terminal colors */
