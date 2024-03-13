36dc798c728ea02704cf243ce19ad29be0d5d2b9
04burhanuddin <dev.burhanuddin@gmail.com>
2024-03-14 02:12:27 +0700

    change borderpx & key zoomin & zoomout

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetbrainsMono Nerd Font:pixelsize=15:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font:pixelsize=15:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	/* add font you need on terminal */
@@ -13,7 +13,7 @@ static char *font2[] = {
   "noto-fonts-emoji:pixelsize=12:antialias=true:autohint=true"
 };
 
-static int borderpx = 10;
+static int borderpx = 8;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -265,8 +265,8 @@ static Shortcut shortcuts[] = {
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
+	{ TERMMOD,              XK_Z,           zoom,           {.f = +1} },
+	{ TERMMOD,              XK_A,           zoom,           {.f = -1} },
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
